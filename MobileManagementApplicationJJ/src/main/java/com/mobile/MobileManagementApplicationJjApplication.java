e89de0cb54932b8b98749fdd33d2868f3db34e32
jainjash1999 <52889693+jainjash1999@users.noreply.github.com>
2022-03-25 16:53:25 +0530

    Add files via upload

---
+++ MobileManagementApplicationJJ/src/main/java/com/mobile/MobileManagementApplicationJjApplication.java
@@ -0,0 +1,13 @@
+package com.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileManagementApplicationJjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileManagementApplicationJjApplication.class, args);
+	}
+
+}
